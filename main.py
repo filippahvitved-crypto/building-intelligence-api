2860b29d9b2720f1b0f0c24674d8222c07a14ba9
Filippa Hvitved <filippahvitved@Filippas-MacBook-Air.local>
2026-05-28 08:48:53 +0200

    move bbr helpers to separate module

--- main.py
+++ main.py
@@ -85,6 +85,16 @@ def validate_api_key(api_key):
 #6.Helper functions
 #------------------------------
 
+def map_bbr_heating_code(code):
+    heating_map = {
+        "1": "district_heating",
+        "2": "gas",
+        "3": "electric",
+        "5": "heat_pump",
+        "6": "oil"
+    }
+
+    return heating_map.get(str(code), "unknown")
 
 #------------------------------
 #7.Endpoints
